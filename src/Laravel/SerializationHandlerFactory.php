f0688c58649ce909b5b6f6ab1b35dbbf3b15a39b
Aymane Dara Hlamnach <dara-@outlook.jp>
2023-09-19 10:39:25 +0100

    dark: update laravel handler resolution

--- src/Laravel/SerializationHandlerFactory.php
+++ src/Laravel/SerializationHandlerFactory.php
@@ -3,16 +3,22 @@
 namespace YouCanShop\Cereal\Laravel;
 
 use YouCanShop\Cereal\Contracts\SerializationHandler;
+use YouCanShop\Cereal\DefaultSerializationHandler;
 use YouCanShop\Cereal\SerializationHandlerFactory as BaseSerializationHandlerFactory;
 
 class SerializationHandlerFactory extends BaseSerializationHandlerFactory
 {
     public function getHandler(string $type): SerializationHandler
     {
+        $handler = parent::getHandler($type);
+        if (!$handler instanceof DefaultSerializationHandler) {
+            return $handler;
+        }
+
         if (is_subclass_of($type, \Illuminate\Database\Eloquent\Model::class)) {
             return new ModelSerializationHandler();
         }
 
-        return parent::getHandler($type);
+        return $handler;
     }
 }
